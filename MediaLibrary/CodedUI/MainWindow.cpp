0733df80752327720815d0d1a5d211f192cc5702
Luaidh <Marin.Althuis@t-online.de>
2013-05-16 19:17:45 +0200

    UI coding

--- MediaLibrary/CodedUI/MainWindow.cpp
+++ MediaLibrary/CodedUI/MainWindow.cpp
@@ -11,7 +11,8 @@ MainWindow::Grid_Main::Grid_Main()
 	SetRow(men_main, 0);
 	Children->Add(men_main);
 
-	rd_main_1 = gcnew RowDefinition();
+	rd_main_1 = gcnew RowDefinition;
+	rd_main_1->Height = gcnew GridLength(20, GridUnitType::Pixel);
 	rd_main_2 = gcnew RowDefinition();
 	rd_main_3 = gcnew RowDefinition();
 }
